80a95a9e2b0298ed721d41eef7125a81f53f9ac8
Sabína Samporová <ssamporova@gmail.com>
2022-01-22 13:59:04 +0100

    whole web page

---
+++ footer.js
@@ -0,0 +1,28 @@
+document.write(
+    '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">\n' +
+    '<link rel="stylesheet" href="footer-style.css">\n' +
+    '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\n' +
+    '<div class="container">\n' +
+    '    <div class="row footer">\n' +
+    '        <div class="col-3 col_centering">\n' +
+    '            <a href="https://www.facebook.com/pedikura.miroslava.samporova/" class="fa fa-facebook foot_text fa-2x center_vert"></a>\n' +
+    '        </div>\n' +
+    '\n' +
+    '        <div class="col-3 col_centering">\n' +
+    '            <a href="#" class="fa fa-phone foot_text"><table><tbody><tr><td>0918 571 265</td></tr></tbody></table></a>\n' +
+    '        </div>\n' +
+    '\n' +
+    '        <div class="col-3 col_centering">\n' +
+    '            <a href="https://www.google.com/maps/place/Horn%C3%A9+Ore%C5%A1any+30,+919+03+Horn%C3%A9+Ore%C5%A1any/@48.4646024,17.4385689,21z/data=!4m8!1m2!2m1!1zSG9ybsOpIE9yZcWhYW55IDMw!3m4!1s0x476ca4c22080bd53:0xf82c74fb90bf0482!8m2!3d48.4646767!4d17.438538" class="fa fa-map-marker foot_text">\n' +
+    '                <table>\n' +
+    '                        <tbody>\n' +
+    '                            <tr><td>Horné Orešany, 30</td></tr>\n' +
+    '                        </tbody>\n' +
+    '                </table>\n' +
+    '            </a>\n' +
+    '        </div>\n' +
+    '\n' +
+    '        <div class="col-3 col_centering">\n' +
+    '            <a href="https://svetkrasymarilyn.sk/" class="fa fa-home foot_text"><table><tbody><tr><td>Svet krásy Marilyn</td></tr></tbody></table></a>\n' +
+    '        </div>'
+)
---
+++ nav.js
@@ -0,0 +1,12 @@
+document.write(
+    '<link rel="stylesheet" href="nav-style.css">' +
+    '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">'+
+    '<nav class="nav">\n' +
+    '        <a class="nav-link under-nav offset_nav_link" href="salon.html"><div class="margin-bot">SALÓN</div></a>\n' +
+    '        <a class="nav-link under-nav" href="services.html"><div class="margin-bot">SLUŽBY</div></a>\n' +
+    '        <a class="nav-link under-nav" href="pricing.html"><div class="margin-bot">CENNÍK</div></a>\n' +
+    '        <a class="nav-link under-nav" href="certificates.html"><div class="margin-bot">OSVEDČENIA</div></a>\n' +
+    '        <a class="nav-link under-nav" href="about.html"><div class="margin-bot">O MNE</div></a>\n' +
+    '        <a class="nav-link under-nav" href="contact.html"><div class="margin-bot">KONTAKT</div></a>\n' +
+    '    </nav>'
+    );
